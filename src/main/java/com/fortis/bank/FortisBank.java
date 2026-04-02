6e81f326b28bb57b3c4adb2e6a6f1d8a2e6457a1
Franck Merlin <merlinkayeu@gmail.com>
2026-04-01 20:32:17 -0400

    chore: initialize project skeleton

---
+++ src/main/java/com/fortis/bank/FortisBank.java
@@ -0,0 +1,18 @@
+package com.fortis.bank;
+
+/**
+ * Entry point for the Fortis Bank console application.
+ *
+ * @author Franck Merlin
+ * @version v0.0.1
+ */
+public final class FortisBank {
+
+    private FortisBank() {
+        // Utility class; no instances.
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Fortis Bank System - MVP bootstrap");
+    }
+}
